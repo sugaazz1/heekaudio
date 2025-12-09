57f77e56b28f37d69ea84085aad8b75b1e153863
Caleb Ogboe <cogboe@brooklynsteamcenter.org>
2025-12-09 10:50:43 -0500

    start1.1_AddingProducts

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 from flask import Flask, render_template
 
+import pymysql
+
+from dynaconf import Dynaconf
+
+
 app = Flask(__name__)
+
+config = Dynaconf(settings_file =["settings.toml"])
+
+def connect_db():
+    conn = pymysql.connect(
+        host="db.steamcenter.tech",
+        user="cogboe",
+        password = config.password,
+        database="cogboe_heekaudio",
+        autocommit= True,
+        cursorclass= pymysql.cursors.DictCursor
+    )
+    return conn
+
 
 @app.route("/")
 def index():
     return render_template("homepage.html.jinja")
+
+@app.route("/browse")
+def browse():
+    connection = connect_db()
+
+    cursor = connection.cursor()
+    
+    cursor.execute("SELECT * FROM `Product` ")
+
+    result = cursor.fetchall()
+
+    connection.close()
+    return render_template("browse.html.jinja")
+
